c1737944398ff7fc050978b1764aff84104e34f8
Mike John <eviotamikejohnb@gmail.com>
2023-05-26 13:36:52 +0800

    g
    g

--- src/routes/db/firebase.js
+++ src/routes/db/firebase.js
@@ -1,18 +1,24 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDVDXDNSOmS0_qdHQVDIu4Q-i70XHY1J-s",
-  authDomain: "barangaysystem-699c2.firebaseapp.com",
-  projectId: "barangaysystem-699c2",
-  storageBucket: "barangaysystem-699c2.appspot.com",
-  messagingSenderId: "767477025051",
-  appId: "1:767477025051:web:d6f5a98c967ca20a8eebbe"
+  apiKey: "AIzaSyDSzKdyIc8d9Ica73Vbtq7ssVbkIMzcX1c",
+  authDomain: "barangaysn3.firebaseapp.com",
+  projectId: "barangaysn3",
+  storageBucket: "barangaysn3.appspot.com",
+  messagingSenderId: "973238776470",
+  appId: "1:973238776470:web:4418abb366fcd442ef88b7"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+
