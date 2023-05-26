d1ab6c57ff61f0fd5301e4b34aad69b0941d4333
Mike John <eviotamikejohnb@gmail.com>
2023-05-26 15:16:30 +0800

    g
    g

--- src/routes/ComponentsLib/LiveComponents/DashBoard.svelte
+++ src/routes/ComponentsLib/LiveComponents/DashBoard.svelte
@@ -76,40 +76,40 @@
     <div>
         
         <div class="text-xl font-bold text-white absolute p-4 bg-black rounded-lg m-2">{counter}</div>
-        <div class="max-w-[250px] min-h-[25vh] bg-purple-500">
+        <div class="max-w-[250px] min-h-[25vh] bg-[#333333]">
             
-            <p class="text-xl font-bold p-16 cursor-not-allowed">REGISTERED VOTERS </p>
+            <p class="text-xl font-bold p-16 cursor-not-allowed text-white">REGISTERED VOTERS </p>
         </div>
     </div>
 
     <!--TOTAL ID REQUEST-->
     <div>
         <div class="text-xl font-bold text-white absolute p-4 bg-black rounded-lg m-2">{counter2}</div>
-        <div class="max-w-[250px] min-h-[25vh] bg-purple-500">
-            <p class="text-xl font-bold p-16 cursor-not-allowed">TOTAL BARANGAY ID REQUEST</p>
+        <div class="max-w-[250px] min-h-[25vh] bg-[#333333]">
+            <p class="text-xl font-bold p-16 cursor-not-allowed text-white">TOTAL BARANGAY ID REQUEST</p>
         </div>
     </div>
     <!--TOTAL CERTIFICATE REQUEST-->
     <div>
         <div class="text-xl font-bold text-white absolute p-4 bg-black rounded-lg m-2">{counter3}</div>
-        <div class="max-w-[250px] min-h-[25vh] bg-purple-500">
-            <p class="text-xl font-bold p-16 cursor-not-allowed">TOTAL BARANGAY CERTIFICATE REQUEST</p>
+        <div class="max-w-[250px] min-h-[25vh] bg-[#333333]">
+            <p class="text-xl font-bold p-16 cursor-not-allowed text-white">TOTAL BARANGAY CERTIFICATE REQUEST</p>
         </div>
     </div>
 
     <!--TOTAL CLEARANCE REQUEST-->
     <div>
         <div class="text-xl font-bold text-white absolute p-4 bg-black rounded-lg m-2">{counter4}</div>
-        <div class="max-w-[250px] min-h-[25vh] bg-purple-500">
-            <p class="text-xl font-bold p-16 cursor-not-allowed">TOTAL CLEARANCE REQUEST</p>
+        <div class="max-w-[250px] min-h-[25vh] bg-[#333333]">
+            <p class="text-xl font-bold p-16 cursor-not-allowed text-white">TOTAL CLEARANCE REQUEST</p>
         </div>
     </div>
 
     <!--TOTAL OF COMPLAINT-->
     <div>
         <div class="text-xl font-bold text-white absolute p-4 bg-black rounded-lg m-2">{counter5}</div>
-        <div class="max-w-[250px] min-h-[25vh] bg-purple-500">
-            <p class="text-xl font-bold p-16 cursor-not-allowed">TOTAL OF COMPLAINT</p>
+        <div class="max-w-[250px] min-h-[25vh] bg-[#333333]">
+            <p class="text-xl font-bold p-16 cursor-not-allowed text-white">TOTAL OF COMPLAINT</p>
         </div>
     </div>
 </div>
